02d8a90a69d5a6c9a29e704361dcb5456ee65ab9
Miguel Palau <mpalauzarza@gmail.com>
2018-02-27 14:17:14 -0600

    2 bundles aka double fun

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,9 @@ if (shouldBeDebugMode) {
 }
 
 const normalConfig = {
-  entry: paths.entry,
+  entry: {
+    legacy: paths.entry
+  },
   devtool: shouldBeDebugMode ? 'source-map' : 'inline-source-map',
   module: {
     rules: [
@@ -67,8 +69,86 @@ const normalConfig = {
       }
     ]},
   output: {
-    filename: shouldBeDebugMode ? '[name].min.js' : '[name].js',
-    chunkFilename: shouldBeDebugMode ? '[name].chunk.min.js' : '[name].chunk.js',
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    path: paths.output,
+    publicPath: '/'
+  },
+  plugins,
+  externals: shouldBeDebugMode ? {
+    jquery: 'jQuery'
+  } : {},
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  },
+  resolve: {
+    alias: {
+      styles: paths.styles
+    }
+  },
+  bail: shouldBeDebugMode,
+  mode: production ? 'production' : 'development'
+}
+
+const es6Config = {
+  entry: {
+    main: paths.entry
+  },
+  devtool: shouldBeDebugMode ? 'source-map' : 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|mjs)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              eslintPath: require.resolve('eslint'),
+              baseConfig: {
+                extends: [require.resolve('@pixel2html/eslint-config')]
+              },
+              ignore: false,
+              useEslintrc: false
+            },
+            loader: require.resolve('eslint-loader')
+          }
+        ],
+        include: paths.src
+      },
+      {
+        test: /\.(js|jsx|mjs)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/env', {
+                modules: false,
+                useBuiltIns: false,
+                targets: {
+                  browsers: [
+                    'Chrome >= 60',
+                    'Safari >= 10.1',
+                    'iOS >= 10.3',
+                    'Firefox >= 54',
+                    'Edge >= 15',
+                  ],
+                },
+              }]
+            ],
+            cacheDirectory: true
+          }
+        }
+      }
+    ]},
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
     path: paths.output,
     publicPath: '/'
   },
@@ -95,5 +175,6 @@ const normalConfig = {
 }
 
 module.exports = [
-  normalConfig
+  normalConfig,
+  es6Config
 ]
